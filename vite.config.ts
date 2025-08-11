6902707b9fdbec456f84408b78da70523b9d2a6d
Colton Santiago <coltonsantiago298@gmail.com>
2025-08-11 12:29:26 -0400

    Use root base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // ← use "/" if you have a custom domain on this repo
+  base: "/", // custom domain = root
 });
+
